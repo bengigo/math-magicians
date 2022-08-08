c841093b321217b8174d31606c891b0c789cebae
bengigenc <genc.bengi@gmail.com>
2022-08-08 16:57:24 +0300

    Add greeting text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello all math-lovers.
         </p>
+        <p>Math Magicians. Coming soon...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
