f12a2d7d650adcdd2d02c7714965b06dd757d760
bengigenc <genc.bengi@gmail.com>
2022-08-08 17:02:07 +0300

    Update greeting message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello all math-lovers.</p>
-        <p>Math Magicians. Coming soon...</p>
+        <p>Math Magicians 🪄 Coming soon...</p>
       </header>
     </div>
   );
