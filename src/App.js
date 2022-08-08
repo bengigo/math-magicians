b990400806318c7b173cc3dc35bb914c4d35a322
bengigenc <genc.bengi@gmail.com>
2022-08-08 16:58:19 +0300

    Remove link provided by template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello all math-lovers.
-        </p>
+        <p>Hello all math-lovers.</p>
         <p>Math Magicians. Coming soon...</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
