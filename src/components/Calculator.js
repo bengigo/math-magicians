8a396c6a50aca91516a8d2b305c1e5e1429d3e19
bengigenc <genc.bengi@gmail.com>
2022-08-10 12:04:46 +0300

    Create design template

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,26 +3,26 @@ import React from 'react';
 function Calculator() {
   return (
     <div className="ui">
-      <div className="output">0</div>
-      <div className="clear">AC</div>
-      <div className="sign">+/-</div>
-      <div className="percent">%</div>
-      <div className="operand">÷</div>
-      <div className="num">7</div>
-      <div className="num">8</div>
-      <div className="num">9</div>
-      <div className="operand">x</div>
-      <div className="num">4</div>
-      <div className="num">5</div>
-      <div className="num">6</div>
-      <div className="operand">-</div>
-      <div className="num">1</div>
-      <div className="num">2</div>
-      <div className="num">3</div>
-      <div className="operand">+</div>
-      <div className="num">0</div>
-      <div className="point">.</div>
-      <div className="equals">=</div>
+      <div className="output cell">0</div>
+      <div className="clear cell">AC</div>
+      <div className="sign cell">+/-</div>
+      <div className="percent cell">%</div>
+      <div className="operand cell">÷</div>
+      <div className="num cell">7</div>
+      <div className="num cell">8</div>
+      <div className="num cell">9</div>
+      <div className="operand cell">x</div>
+      <div className="num cell">4</div>
+      <div className="num cell">5</div>
+      <div className="num cell">6</div>
+      <div className="operand cell">-</div>
+      <div className="num cell">1</div>
+      <div className="num cell">2</div>
+      <div className="num cell">3</div>
+      <div className="operand cell">+</div>
+      <div className="num zero cell">0</div>
+      <div className="point cell">.</div>
+      <div className="equals cell">=</div>
     </div>
   );
 }
