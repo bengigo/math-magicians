a53e91986291909d4e99a1da09d18db0b7301318
bengigenc <genc.bengi@gmail.com>
2022-08-10 11:41:21 +0300

    Create UI skeleton

---
+++ src/components/Calculator.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Calculator() {
+  return (
+    <div className="ui">
+      <div className="output">0</div>
+      <div className="clear">AC</div>
+      <div className="sign">+/-</div>
+      <div className="percent">%</div>
+      <div className="operand">÷</div>
+      <div className="num">7</div>
+      <div className="num">8</div>
+      <div className="num">9</div>
+      <div className="operand">x</div>
+      <div className="num">4</div>
+      <div className="num">5</div>
+      <div className="num">6</div>
+      <div className="operand">-</div>
+      <div className="num">1</div>
+      <div className="num">2</div>
+      <div className="num">3</div>
+      <div className="operand">+</div>
+      <div className="num">0</div>
+      <div className="point">.</div>
+      <div className="equals">=</div>
+    </div>
+  );
+}
+
+export default Calculator;
