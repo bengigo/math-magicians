98e2e7792fefd5576357a9e625f9f59caf503fcc
bengigenc <genc.bengi@gmail.com>
2022-08-16 21:44:38 +0300

    Style and position quote page elements accorging to wireframe

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -11,10 +11,12 @@ const Quote = () => (
         <Link className="link" to="/quote">Quote</Link>
       </nav>
     </header>
-    <p>
-      Mathematics is a language developed by humanity to understand nature and
-      use it for its own benefit. -Pınar Ezgi Kadı
-    </p>
+    <div className="quote-section">
+      <p className="quote">
+        Mathematics is a language developed by humanity to understand nature and
+        use it for its own benefit. -Pınar Ezgi Kadı
+      </p>
+    </div>
   </main>
 );
 
